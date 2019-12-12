44934e3bdff5a2b03af183e732d38f15e59a7934
Viknesh Ramm <vikneshramm@outlook.com>
2019-12-12 21:48:57 +0530

    Creation of home page is completed
    Google sign in feature is still not implemented
    
    Signed-off-by: Viknesh Ramm <vikneshramm@outlook.com>

--- gatekeeper/src/app/app.module.ts
+++ gatekeeper/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
