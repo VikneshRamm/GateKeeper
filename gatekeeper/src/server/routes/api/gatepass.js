2e253afe5ccc9251c962ecd2644b8e5d4622fc24
sudharsan <sud6har2san@gmail.com>
2020-01-03 00:14:50 +0530

    feat(server): Google Sign In - Server Side Validation - In progress

--- gatekeeper/src/server/routes/api/gatepass.js
+++ gatekeeper/src/server/routes/api/gatepass.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const dbConnect = require('../../db/dbConnect');
+// const auth = require('../auth.js');
+
+const {OAuth2Client} = require('google-auth-library');
+
+
 
 // const sequelize = dbConnect.sequelize;
 // const logger = require();
@@ -17,6 +22,43 @@ router.get('/', (req, res) => {
 router.post('/add', (req, res) => {
     dbConnect.addUser();
     res.json({msg: "Success!"});
+});
+
+router.post('/token_id', (req, res) => {
+    var userDetails;
+
+    if (!req.body.token)
+    {
+        res.status(400).json({msg: "Token Missing."});
+    }
+
+    const CLIENT_ID = '819801723091-c220n12ttnnvbrdojs2m281svp52flfq.apps.googleusercontent.com';
+
+    const client = new OAuth2Client(CLIENT_ID);
+
+    async function verify(req, res) {
+        const ticket = await client.verifyIdToken({
+            idToken: req.body.token,
+            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+            // Or, if multiple clients access the backend:
+            //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+        });
+        const payload = ticket.getPayload();
+        const userid = payload['sub'];
+        // If request specified a G Suite domain:
+        const domain = payload['hd'];
+
+        res.json({
+            userid: userid,
+            domain: domain,
+            email: payload['email']
+        }
+        );
+    }
+
+    verify(req, res);
+    // auth.googAuthTokenVerify(req.body.token, userDetails);
+    // res.json({msg: "Success!"});
 });
 
 module.exports = router;
